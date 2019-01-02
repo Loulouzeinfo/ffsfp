a4af8fd07c6bf22fc71743b2389fc33958b2f834
CHIKHOUNE MOULOUD <chikhouneloulouze@gmail.com>
2019-01-02 23:52:41 +0100

    testecommande

--- Administrateur/accueil.php
+++ Administrateur/accueil.php
@@ -1,79 +1,69 @@
-<?php  
- session_start();
- include("../DB/base.php");
- include("../Fonction/fonction.php");
-$tab=array();
-$profile='';
-$v1='';
+<?php
+session_start();
+include '../DB/base.php';
+include '../Fonction/fonction.php';
+$tab = array();
+$profile = '';
+$v1 = '';
 
-if(!isset($_SESSION['login'])){ 
-header("Location:../index.php");
-
-}else{
-
-
-  if(isset($_SESSION['action']) AND (time()-$_SESSION['action'])<300 ){
-
-    $_SESSION['action'] = time();
-
-
-  }else{
-
-         $v1="<script>
+if (!isset($_SESSION['login'])) {
+    header('Location:../index.php');
+} else {
+    if (isset($_SESSION['action']) and (time() - $_SESSION['action']) < 300) {
+        $_SESSION['action'] = time();
+    } else {
+        $v1 = '<script>
                swal({
-                
-                text: \"Vous êtes déconnecté !\",
-                icon: \"info\"
+
+                text: "Vous êtes déconnecté !",
+                icon: "info"
                 }).then(function() {
-                window.location = \"../index.php\";
+                window.location = "../index.php";
                  });
-               
-               </script>";
 
-  }
-  $sess=$_SESSION['login'];
-  $sql2= "SELECT * FROM personne WHERE mail='$sess'";
-  $donpro= $mysqli->query($sql2)or die(mysqli_error($mysqli));
-  $respro= $donpro->fetch_array();
-  $profile=$respro['prenom'];
+               </script>';
+    }
+    $sess = $_SESSION['login'];
+    $sql2 = "SELECT * FROM personne WHERE mail='$sess'";
+    $donpro = $mysqli->query($sql2) or die(mysqli_error($mysqli));
+    $respro = $donpro->fetch_array();
+    $profile = $respro['prenom'];
 
-  $sql="SELECT * FROM personne";
-  $donn=$mysqli->query($sql)or die(mysqli_error($mysqli));
-  while($res= $donn->fetch_array()){
-     $tab[]=$res;
-  }
+    $sql = 'SELECT * FROM personne';
+    $donn = $mysqli->query($sql) or die(mysqli_error($mysqli));
+    while ($res = $donn->fetch_array()) {
+        $tab[] = $res;
+    }
 }
 
-
-
-  ?>
+?>
 
 
 <!doctype html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  
-  <?php  include("../Blocs_HTML/script_bootstrap_header.php");  ?>
+
+  <?php include '../Blocs_HTML/script_bootstrap_header.php'; ?>
   <title>accueil admin</title>
   <link rel="stylesheet" href="../CSS/Style.css">
 
 <body>
 
- <?php    include("../Blocs_HTML/nav.php"); ?>
+ <?php include '../Blocs_HTML/nav.php'; ?>
 
 
 <div class="jumbotron accueil">
   <div class="container">
     <h1 class="display-4">Accueil</h1>
-    
+
   </div>
 </div>
 
-<?php   echo $v1; ?>
+<?php echo $v1; ?>
 
 
-<?php    include("../Blocs_HTML/footer.php"); ?>
+<?php include '../Blocs_HTML/footer.php';?>
 
 
 </body>
